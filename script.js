06e4739ecb002289a6ff3a8aae4b6c48fae5c3ca
AngeloDulpina <150144514+AngeloDulpina@users.noreply.github.com>
2023-12-11 17:09:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function toggleText(event)
+{       
+    var text = event.textContent || event.innerText;
+        if(text == 'Facebook') 
+    {
+     event.innerHTML ='FB: Angelo Dulpina';                   ;
+    }
+    else
+    {
+    event.innerHTML = 'Facebook';
+    }
+
+}
+
+function toggleText(event)
+{       
+    var text = event.textContent || event.innerText;
+        if(text == 'Instagram') 
+    {
+     event.innerHTML ='IG: aben.angelo';                   ;
+    }
+    else
+    {
+    event.innerHTML = 'Instagram';
+    }
+
+}
+
+
+
